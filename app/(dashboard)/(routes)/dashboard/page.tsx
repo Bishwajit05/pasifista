9d4709cee02ee9bced4cc5fffdc81d2472afaf70
Bishwajit Behera <bbishwajit05@gmail.com>
2023-07-28 15:28:07 +0530

    Sidebar

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <p>Dashboard page (protected)</p>
+    </div>
+  );
+}
